6b8b37c4e75b0069f2826556e82cb5659c2c7a2e
MicraP-TR <oyuncuhesabimxd40@gmail.com>
2025-10-19 10:53:13 +0300

    Update MicraP++ (25.2).h
    API düzeltmeleri

--- MicraP++ (25.2).h
+++ MicraP++ (25.2).h
@@ -13,32 +13,12 @@
 #define YAPI(name) struct name
 #define BİTİR printf("\n \nPROGRAM BASARIYLA BITTI. KOD: 0 \n")
 
-struct data {
-    int bayt;
-};
-
-struct data VERI;
-
-typedef struct {
-    const short int libkod[2];
-    char durum[2];
-} BAGLANTI;
-
-BAGLANTI API = {
-    .libkod = {0xA0C0, 0xBC00},
-    .durum = {'Y', 'Y'}
-};
-
-inline void YAZ(char yazma) {
-    if (yazma != 0) {
-        printf("%c", yazma);
-    }
+static void YAZ(char yazma) {
+    if (yazma != 0) printf("%c", yazma);
 }
 
-inline void OKU(char *okuma) {
-    if (okuma != NULL) {
-        scanf(" %c", okuma);
-    }
+static void OKU(char *okuma) {
+    if (okuma != NULL) scanf(" %c", okuma);
 }
 
 inline void TAŞI(int *tasinan, int *tasiyan) {
@@ -56,15 +36,5 @@ inline void EKLE(int *hedef, int deger) {
 inline void BÖL(int bolunen, int bolen, int *kalan) {
     if (bolen != 0 && kalan) {
         *kalan = bolunen % bolen;
-    }
-}
-
-inline void KÜTÜPHANE(int libkodu, char durum) {
-    if (libkodu != 0 && (durum == 'Y' || durum == 'D')) {
-        if (API.libkod[0] == libkodu) {
-            API.durum[0] = durum;
-        } else if (API.libkod[1] == libkodu) {
-            API.durum[1] = durum;
-        }
     }
 }
